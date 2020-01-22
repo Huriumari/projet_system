b7fab6e5ab43c16492917bc609f27227d75869ef
huriumari <clementbossard@free.fr>
2020-01-22 08:08:33 -0800

    question 1

---
+++ include/superpixmanip.h
@@ -0,0 +1,15 @@
+#ifndef SUPERPIXMANIP_H
+# define SUPERPIXMANIP_H
+
+# include <stdint.h>
+# include <unistd.h>
+
+typedef struct	pixel_s
+{
+	uint8_t	red;
+	uint8_t	blue;
+	uint8_t	green;
+}				pixel_f;
+
+
+#endif
---
+++ source/main.c
@@ -0,0 +1,5 @@
+#include "superpixmanip.h"
+
+int		main(void){
+	return 0;
+}
