cbcd5be32c4063ff87c249362b574a694b6ed935
huriumari <clementbossard@free.fr>
2020-01-22 08:34:33 -0800

    question 3

--- include/superpixmanip.h
+++ include/superpixmanip.h
@@ -4,6 +4,18 @@
 # include <stdint.h>
 # include <unistd.h>
 
+//stdbool.h: no such file or directory
+//
+//# include <stdbool.h.>
+//
+//
+//on remplace par un type def
+typedef enum
+{
+    false = 0,
+    true  = 1,
+} bool;
+
 typedef struct	pixel_s
 {
 	uint8_t	red;
--- source/functions_img.c
+++ source/functions_img.c
@@ -11,3 +11,13 @@ uint8_t	img_green(const pixel_t *p){
 uint8_t	img_red(const pixel_t *p){
 	return p->red;
 }
+
+bool	img_cmp(const pixel_t *p1, const pixel_t *p2){
+	if (p1->green != p2->green)
+		return false;
+	if (p1->red != p2->red)
+		return false;
+	if (p1->blue != p2->blue)
+		return false;
+	return true;
+}
